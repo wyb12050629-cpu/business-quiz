8cc775880e624b90491a6f964e0331b423b03e83
yubin <wyb12050629@gmail.com>
2026-05-14 22:42:04 +0000

    fix: update displayName to Korean

--- granite.config.ts
+++ granite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@apps-in-toss/web-framework/config';
 export default defineConfig({
   appName: 'business-quiz',
   brand: {
-    displayName: 'business-quiz', // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
+    displayName: '직장인 상식 퀴즈',
     primaryColor: '#2780FA', // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
     icon: 'https://static.toss.im/appsintoss/34591/7ab7c17e-33f0-4445-ac58-6b6113310c83.png', // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
   },
